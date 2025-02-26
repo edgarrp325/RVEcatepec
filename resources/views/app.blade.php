91bdf9c95a8b8145ce220ce84a7a4c244cf50543
edgarrp325 <edgar.rp325@gmail.com>
2025-02-26 13:09:57 -0600

    Change appName and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Realidad Virtual C.U. UAEM Ecatepec') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
