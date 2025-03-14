de2e1f30f9ce1c37b1bda2541738fd224224ec62
edgarrp325 <edgar.rp325@gmail.com>
2025-03-14 14:06:59 -0600

    Change registration logic to fit the required roles

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\MajorEnum;
+use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
+use App\Models\Major;
+use App\Models\OTP;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -20,7 +26,10 @@ class RegisteredUserController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('auth/register');
+        return Inertia::render('auth/register', [
+            'roles' => Role::all(),
+            'majors' => Major::all(),
+        ]);
     }
 
     /**
@@ -30,22 +39,83 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        $isSelectedUser = $request->role_id == RoleEnum::USER->value;
+        $isSelectedAdmin = $request->role_id == RoleEnum::ADMIN->value;
 
-        $user = User::create([
+        $isSelectedOtherMajor = $request->major_id == MajorEnum::OTHER->value;
+
+        //Validate the admin code if is not a normal user
+        if (!$isSelectedUser) {
+            $otpType = $isSelectedAdmin ? 'admin_code' : 'user_code';
+            $request->validate([
+                'code' => [
+                    'required',
+                    'string',
+                    'size:6',
+                    Rule::exists(OTP::class, 'code')->where('id', $otpType),
+                ],
+            ]);
+        }
+
+        //Common validation rules for all roles
+        $rules = [
+            'paternal_surname' => 'required|string|max:255',
+            'maternal_surname' => 'nullable|string|max:255',
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'role_id' => 'required|exists:' . Role::class . ',id',
+        ];
+
+        //Add specific validation rules for each role
+
+        // if is user or is selected other major validate the origin
+        if ($isSelectedUser || $isSelectedOtherMajor) {
+            $rules['origin'] = 'required|string|max:255';
+        } else {
+            $rules['major_id'] = 'required|exists:' . Major::class . ',id';
+        }
+
+
+        // All roles except USER require an account number
+        if (!$isSelectedUser) {
+            $rules['account_number'] = 'required|string|max:7|unique:' . User::class;
+        }
+
+        //Validate the request
+        $request->validate($rules);
+
+        //Prepare common data to create the user
+        $data = [
+            'paternal_surname' => $request->paternal_surname,
+            'maternal_surname' => $request->maternal_surname,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ]);
+            'role_id' => $request->role_id,
+        ];
+
+        //Add specific data for each role
+        // All roles except USER require an account number
+        if (!$isSelectedUser) {
+            $data['account_number'] = $request->account_number;
+        }
+
+        //Create the user
+        $user = User::create($data);
 
         event(new Registered($user));
 
+        //Add origin if is user or is selected other major
+        if ($isSelectedUser || $isSelectedOtherMajor) {
+            $user->origin()->create(['name' => $request->origin]);
+        } else {
+            $user->majors()->attach($request->major_id);
+        }
+
         Auth::login($user);
 
-        return to_route('dashboard');
+        //if rol is user return to the last page before login
+        return $isSelectedUser ? redirect()->intended() : to_route('dashboard');
     }
 }
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -1,6 +1,6 @@
 import { Head, useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
-import { FormEventHandler } from 'react';
+import { FormEventHandler, useEffect, useState } from 'react';
 
 import InputError from '@/components/input-error';
 import TextLink from '@/components/text-link';
@@ -9,27 +9,105 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
 interface RegisterForm {
+    account_number: string;
+    paternal_surname: string;
+    maternal_surname: string;
     name: string;
+    origin: string; // origin or another major
+    major_id: string;
     email: string;
     password: string;
     password_confirmation: string;
+    role_id: string;
+    code: string;
+    [key: string]: string;
 }
 
-export default function Register() {
+interface Role {
+    id: number;
+    name: string;
+    description: string;
+}
+
+interface Major {
+    id: number;
+    name: string;
+}
+
+interface RegisterProps {
+    roles: Role[];
+    majors: Major[];
+}
+
+const RoleEnum = {
+    ADMIN: '1',
+    USER: '2',
+    ALUMN: '3',
+    SOCIALSERVICE: '4',
+    INTERNSHIP: '5',
+} as const;
+
+const MajorEnum = {
+    ICO: '1',
+    LIA: '2',
+    LPS: '3',
+    LAM: '4',
+    LCN: '5',
+    LDE: '6',
+    OTHER: '7',
+} as const;
+
+export default function Register({ roles, majors }: RegisterProps) {
     const { data, setData, post, processing, errors, reset } = useForm<RegisterForm>({
+        account_number: '',
+        paternal_surname: '',
+        maternal_surname: '',
         name: '',
+        origin: '',
+        major_id: '',
         email: '',
         password: '',
         password_confirmation: '',
+        role_id: RoleEnum.USER,
+        code: '',
     });
+
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const isSelectedUser = data.role_id === RoleEnum.USER;
+    const isSelectedOtherMajor = data.major_id === MajorEnum.OTHER;
 
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
+
+        if (isSelectedUser) {
+            return sendRegister();
+        }
+        setIsDialogOpen(true);
+    };
+
+    const sendRegister = () => {
         post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
+            onFinish: () => reset('code'),
+            onSuccess: () => reset('password', 'password_confirmation'),
         });
     };
+
+    const handleRoleChange = (value: string) => {
+        setData('role_id', value);
+        if (value === RoleEnum.USER) {
+            setData('major_id', '');
+        }
+    };
+
+    useEffect(() => {
+        setIsDialogOpen(!!errors.code);
+    }, [errors]);
 
     return (
         <AuthLayout title="Create an account" description="Enter your details below to create your account">
@@ -37,13 +115,71 @@ export default function Register() {
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
+                        <Label htmlFor="roles">Type of user </Label>
+                        <Select value={data.role_id.toString()} onValueChange={(value) => handleRoleChange(value)}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select the type of user you need" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {roles.map((role) => (
+                                    <SelectItem key={role.id} value={role.id.toString()}>
+                                        {role.name.charAt(0).toUpperCase() + role.name.slice(1)}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                            <small>{'Description: ' + roles[parseInt(data.role_id) - 1].description}</small>
+                        </Select>
+                    </div>
+                    {!isSelectedUser && (
+                        <div className="grid gap-2">
+                            <Label htmlFor="account_number">Account Number UAEM</Label>
+                            <Input
+                                id="account_number"
+                                type="text"
+                                autoComplete="account_number"
+                                value={data.account_number}
+                                onChange={(e) => setData('account_number', e.target.value)}
+                                disabled={processing}
+                                placeholder="max 7 characters"
+                            />
+                            <InputError message={errors.account_number} className="mt-2" />
+                        </div>
+                    )}
+                    <div className="grid gap-2">
+                        <Label htmlFor="paternal_surname">Paternal surname</Label>
+                        <Input
+                            id="paternal_surname"
+                            type="text"
+                            required
+                            autoComplete="paternal_surname"
+                            value={data.paternal_surname}
+                            onChange={(e) => setData('paternal_surname', e.target.value)}
+                            disabled={processing}
+                            placeholder="Paternal surname"
+                        />
+                        <InputError message={errors.paternal_surname} className="mt-2" />
+                    </div>
+
+                    <div className="grid gap-2">
+                        <Label htmlFor="maternal_surname">Maternal surname</Label>
+                        <Input
+                            id="maternal_surname"
+                            type="text"
+                            autoComplete="maternal_surname"
+                            value={data.maternal_surname}
+                            onChange={(e) => setData('maternal_surname', e.target.value)}
+                            disabled={processing}
+                            placeholder="Maternal surname"
+                        />
+                        <InputError message={errors.maternal_surname} className="mt-2" />
+                    </div>
+
+                    <div className="grid gap-2">
                         <Label htmlFor="name">Name</Label>
                         <Input
                             id="name"
                             type="text"
                             required
-                            autoFocus
-                            tabIndex={1}
                             autoComplete="name"
                             value={data.name}
                             onChange={(e) => setData('name', e.target.value)}
@@ -53,13 +189,52 @@ export default function Register() {
                         <InputError message={errors.name} className="mt-2" />
                     </div>
 
+                    {!isSelectedUser && (
+                        <div className="grid gap-2">
+                            <Label htmlFor="majors">Major</Label>
+                            <Select value={data.major_id.toString()} onValueChange={(value) => setData('major_id', value)}>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select the major you are in" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {majors.map((major) => (
+                                        <SelectItem key={major.id} value={major.id.toString()}>
+                                            {major.name.charAt(0).toUpperCase() + major.name.slice(1)}
+                                        </SelectItem>
+                                    ))}
+                                    <SelectItem key={MajorEnum.OTHER} value={MajorEnum.OTHER}>
+                                        Other
+                                    </SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    )}
+
+                    {(isSelectedUser || isSelectedOtherMajor) && (
+                        <div className="grid gap-2">
+                            {!isSelectedOtherMajor && <Label htmlFor="origin">Origin</Label>}
+                            <Input
+                                id="origin"
+                                type="text"
+                                required
+                                autoComplete="origin"
+                                value={data.origin}
+                                onChange={(e) => setData('origin', e.target.value)}
+                                disabled={processing}
+                                placeholder={
+                                    !isSelectedOtherMajor ? 'School, Faculty, Organization, etc.' : 'Write your school and major (UNAM - Computación)'
+                                }
+                            />
+                            <InputError message={errors.origin} className="mt-2" />
+                        </div>
+                    )}
+
                     <div className="grid gap-2">
                         <Label htmlFor="email">Email address</Label>
                         <Input
                             id="email"
                             type="email"
                             required
-                            tabIndex={2}
                             autoComplete="email"
                             value={data.email}
                             onChange={(e) => setData('email', e.target.value)}
@@ -75,7 +250,6 @@ export default function Register() {
                             id="password"
                             type="password"
                             required
-                            tabIndex={3}
                             autoComplete="new-password"
                             value={data.password}
                             onChange={(e) => setData('password', e.target.value)}
@@ -91,7 +265,6 @@ export default function Register() {
                             id="password_confirmation"
                             type="password"
                             required
-                            tabIndex={4}
                             autoComplete="new-password"
                             value={data.password_confirmation}
                             onChange={(e) => setData('password_confirmation', e.target.value)}
@@ -101,18 +274,37 @@ export default function Register() {
                         <InputError message={errors.password_confirmation} />
                     </div>
 
-                    <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
+                    <Button type="submit" className="mt-2 w-full" disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
                         Create account
                     </Button>
                 </div>
 
                 <div className="text-muted-foreground text-center text-sm">
-                    Already have an account?{' '}
-                    <TextLink href={route('login')} tabIndex={6}>
-                        Log in
-                    </TextLink>
+                    Already have an account? <TextLink href={route('login')}>Log in</TextLink>
                 </div>
+
+                <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Confirmation Code</DialogTitle>
+                            <DialogDescription>Contact the administrator to get the confirmation code</DialogDescription>
+                        </DialogHeader>
+                        <div className="w-full flex-col items-center justify-center gap-2 p-4">
+                            <InputOTP value={data.code} onChange={(value) => setData('code', value)} maxLength={6} onComplete={sendRegister}>
+                                <InputOTPGroup>
+                                    <InputOTPSlot index={0} />
+                                    <InputOTPSlot index={1} />
+                                    <InputOTPSlot index={2} />
+                                    <InputOTPSlot index={3} />
+                                    <InputOTPSlot index={4} />
+                                    <InputOTPSlot index={5} />
+                                </InputOTPGroup>
+                            </InputOTP>
+                            <InputError message={errors.code} />
+                        </div>
+                    </DialogContent>
+                </Dialog>
             </form>
         </AuthLayout>
     );
