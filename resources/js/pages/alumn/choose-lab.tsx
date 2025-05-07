464e9f24f3120a5d1be78aaf8d2fab74e53f1ea4
edgarrp325 <edgar.rp325@gmail.com>
2025-05-07 10:06:22 -0600

    Add visibility of total service hours only for social service and internships

--- resources/js/pages/alumn/choose-lab.tsx
+++ resources/js/pages/alumn/choose-lab.tsx
@@ -1,14 +1,15 @@
-import { Head, useForm } from '@inertiajs/react';
+import { Head, Link, useForm, usePage } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler } from 'react';
 
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import { Laboratory } from '@/types';
+import { Laboratory, SharedData } from '@/types';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { RoleEnum } from '@/enums';
 import { formatMinutes } from '@/lib/utils';
 
 interface ChooseLabForm {
@@ -25,17 +26,35 @@ export default function ChooseLab({ laboratories, totalServiceMinutes }: ChooseL
     const { data, setData, post, processing } = useForm<ChooseLabForm>({
         laboratory_id: 1,
     });
+    const { auth } = usePage<SharedData>().props;
+    const currentRole = auth.user.role_id.toString();
 
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
         post(route('attendance.store'));
     };
+    console.log(currentRole);
 
     return (
         <AuthLayout title="Choose a laboratory" description="Select the laboratory you are going to stay">
             <Head title="Register" />
 
             <form className="flex flex-col gap-6" onSubmit={submit}>
+                {(currentRole === RoleEnum.SOCIALSERVICE || currentRole === RoleEnum.INTERNSHIP) && (
+                    <Card className="w-full max-w-lg border-emerald-200 bg-emerald-50">
+                        <CardHeader>
+                            <CardTitle className="text-xl tracking-tight">Total service hours</CardTitle>
+                        </CardHeader>
+                        <CardContent className="pt-0">
+                            <div className="grid gap-4">
+                                <div>
+                                    <p className="text-muted-foreground text-sm">Total</p>
+                                    <p className="text-2xl font-semibold tabular-nums @[250px]/card:text-5xl">{formatMinutes(totalServiceMinutes)}</p>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )}
                 <div className="grid gap-6">
                     <div className="grid gap-2">
                         <Label htmlFor="laboratories">Laboratory</Label>
@@ -63,21 +82,11 @@ export default function ChooseLab({ laboratories, totalServiceMinutes }: ChooseL
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
                         Continue
                     </Button>
+                    <Link href={route('home')} className={buttonVariants({ variant: 'link' })}>
+                        Go home
+                    </Link>
                 </div>
             </form>
-            <Card className="w-full max-w-lg border-emerald-200 bg-emerald-50">
-                <CardHeader>
-                    <CardTitle className="text-xl tracking-tight">Total service hours</CardTitle>
-                </CardHeader>
-                <CardContent className="pt-0">
-                    <div className="grid gap-4">
-                        <div>
-                            <p className="text-muted-foreground text-sm">Total</p>
-                            <p className="text-2xl font-semibold tabular-nums @[250px]/card:text-5xl">{formatMinutes(totalServiceMinutes)}</p>
-                        </div>
-                    </div>
-                </CardContent>
-            </Card>
         </AuthLayout>
     );
 }
